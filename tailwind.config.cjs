4ecfd591664b58181eb10c219e7db21e03be7ace
Luander Ilidio de Arruda <luander.arruda@unemat.br>
2023-01-09 13:05:06 -0400

    [chore]: adiciona dependencias ao projeto

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Anton: ["Anton", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+        Inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
