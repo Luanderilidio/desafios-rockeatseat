cdf16900083ac0a4366dc6a4f8f0822a57e64277
Luander Ilidio de Arruda <luander.arruda@unemat.br>
2023-01-09 13:04:04 -0400

    [feat]: constroi layout basico do player

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import Image from "../src/assets/image.svg";
+import { FastForward, Play, Rewind } from "phosphor-react";
+import ProgressBar from "@ramonak/react-progress-bar";
+import { IconButton } from "@mui/material";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="sm:fixed w-full h-full flex items-center justify-center p-5 sm:p-0 text-gray-300 bg-[#0F0D13]">
+      <div className="grid grid-cols-2 grid-rows-2 gap-4">
+        <div className="row-span-2 col-span-2 sm:col-span-1 ">
+          <div className="bg-[#2A2141] p-10 flex flex-col gap-8 rounded-lg">
+            <img src={Image} alt="image" className="" />
+            <div>
+              <p className="text-3xl font-bold ">Acorda Devinho</p>
+              <p className="text-lg  text-gray-400">Banda Rockeatseat</p>
+            </div>
+            <div className="flex items-center justify-between ">
+              <IconButton>
+                <Rewind weight="fill" size={32} color="#FFFFFF" />
+              </IconButton>
+              <IconButton>
+                <Play size={32} weight="fill" color="#FFFFFF" />
+              </IconButton>
+              <IconButton>
+                <FastForward weight="fill" size={32} color="#FFFFFF" />
+              </IconButton>
+            </div>
+            <div className="flex flex-col gap-2">
+              <ProgressBar
+                completed={50}
+                height="8px"
+                bgColor="#FFFFFF"
+                baseBgColor="#939393"
+                maxCompleted={100}
+                isLabelVisible={false}
+              />
+              <div className="flex items-center justify-between text-gray-400">
+                <p>00:00</p>
+                <p>03:00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-2 sm:col-span-1 bg-[#2A2141] p-10 flex flex-col gap-6 rounded-lg">
+          <div className="flex gap-4">
+            <img src={Image} alt="image" className="w-20" />
+            <div>
+              <p className="text-2xl font-bold ">Acorda Devinho</p>
+              <p className="text-lg  text-gray-400">Banda Rockeatseat</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-between ">
+            <Rewind weight="fill" size={32} />
+            <Play size={32} weight="fill" />
+            <FastForward weight="fill" size={32} />
+          </div>
+          <div className="flex flex-col gap-2">
+            <ProgressBar
+              completed={50}
+              height="8px"
+              bgColor="#FFFFFF"
+              baseBgColor="#939393"
+              maxCompleted={100}
+              isLabelVisible={false}
+            />
+            <div className="flex items-center justify-between text-gray-400">
+              <p>00:00</p>
+              <p>03:00</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-2 sm:col-span-1 bg-[#2A2141] p-10 flex flex-col gap-6 rounded-lg justify-center">
+          <div className="flex gap-4">
+            <img src={Image} alt="image" className="w-20" />
+            <div>
+              <p className="text-2xl font-bold ">Acorda Devinho</p>
+              <p className="text-lg  text-gray-400">Banda Rockeatseat</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-between ">
+            <Rewind weight="fill" size={32} />
+            <Play size={32} weight="fill" />
+            <FastForward weight="fill" size={32} />
+          </div>
+          {/* <div className="flex flex-col gap-2">
+            <ProgressBar
+              completed={50}
+              height="8px"
+              bgColor="#FFFFFF"
+              baseBgColor="#939393"
+              maxCompleted={100}
+              isLabelVisible={false}
+            />
+            <div className="flex items-center justify-between text-gray-400">
+              <p>00:00</p>
+              <p>03:00</p>
+            </div>
+          </div> */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
